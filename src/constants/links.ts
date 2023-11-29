92bef5ea0ce28ad8a72f25b4b61927449b4d1127
vojin5rovic <vojinp13@gmail.com>
2023-11-29 18:02:37 +0100

    Change twitter link from thalesmarket to thales_io

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,7 +1,7 @@
 const LINKS = {
     Github: 'https://github.com/thales-markets',
     Medium: 'https://thalesmarket.medium.com/',
-    Twitter: 'https://twitter.com/thalesmarket',
+    Twitter: 'https://twitter.com/thales_io',
     Discord: 'https://discord.com/invite/rB3AWKwACM',
     Docs: 'https://docs.thalesmarket.io/',
 };
