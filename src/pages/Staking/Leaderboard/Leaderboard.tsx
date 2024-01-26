579cf4af82d478643932cca0461d01d2ef46e833
vojin5rovic <vojinp13@gmail.com>
2024-01-26 14:35:54 +0100

    Fix leaderboard dropdown

--- src/pages/Staking/Leaderboard/Leaderboard.tsx
+++ src/pages/Staking/Leaderboard/Leaderboard.tsx
@@ -1,6 +1,5 @@
 import TimeRemaining from 'components/TimeRemaining';
 import { USD_SIGN } from 'constants/currency';
-import { ScreenSizeBreakpoint } from 'enums/ui';
 import useStakersDataLeaderboardQuery, {
     StakersWithLeaderboardData,
 } from 'queries/token/useStakersDataLeaderboardQuery';
@@ -9,14 +8,28 @@ import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { getNetworkId, getWalletAddress } from 'redux/modules/wallet';
 import { RootState } from 'redux/rootReducer';
-import styled, { useTheme } from 'styled-components';
+import { useTheme } from 'styled-components';
 import { FlexDiv, FlexDivCentered, FlexDivColumnBottom, FlexDivColumnSpaceBetween } from 'styles/common';
 import { formatCurrencyWithKey, truncateAddress } from 'thales-utils';
 import snxJSConnector from 'utils/snxJSConnector';
 import PeriodDropdown from '../components/PeriodDropdown';
 import { InfoDiv, SectionDescription, SectionTitle } from '../styled-components';
 import Table from './Table';
-import { Bottom, Container, LeaderboardBreakdownTitle, Top } from './styled-components';
+import {
+    Bottom,
+    Container,
+    FlexWrapper,
+    Icon,
+    Label,
+    LeaderboardBreakdownTitle,
+    StickyCell,
+    StickyExpandedRow,
+    StickyRow,
+    StickyRowFlex,
+    StickyRowWrapper,
+    TableText,
+    Top,
+} from './styled-components';
 
 const Leaderboard: React.FC = () => {
     const { t } = useTranslation();
@@ -301,100 +314,5 @@ const StickyRowComponent: React.FC<{ stickyRowInfo: StakersWithLeaderboardData }
         </>
     );
 };
-
-const TableText = styled.p`
-    color: ${(props) => props.theme.textColor.primary};
-    font-size: 14px;
-    font-style: normal;
-    line-height: 110%;
-    @media screen and (max-width: ${ScreenSizeBreakpoint.SMALL}px) {
-        font-size: 13px;
-    }
-`;
-
-const StickyRow = styled.div`
-    display: flex;
-    margin-bottom: 10px;
-    gap: 4px;
-    cursor: pointer;
-
-    ${TableText} {
-        color: ${(props) => props.theme.button.textColor.primary};
-    }
-
-    @media screen and (max-width: ${ScreenSizeBreakpoint.SMALL}px) {
-        height: 30px;
-    }
-`;
-
-const StickyRowWrapper = styled.div`
-    background: ${(props) => props.theme.background.primary};
-    width: 100%;
-    border-radius: 8px;
-    box-shadow: 0px 10px 15px -5px rgba(0, 0, 0, 0.46);
-`;
-
-const StickyRowFlex = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    height: 40px;
-    border-radius: 8px;
-`;
-
-const Icon = styled.i<{ dark?: boolean }>`
-    font-size: 32px;
-    color: ${(props) => (props.dark ? props.theme.background.primary : props.theme.textColor.primary)};
-    margin-right: 6px;
-`;
-
-const FlexWrapper = styled(FlexDivCentered)`
-    flex-direction: column;
-`;
-
-const Label = styled.span<{ dark?: boolean }>`
-    color: ${(props) => (props.dark ? props.theme.background.primary : props.theme.textColor.secondary)};
-    font-size: 13px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 100%;
-    letter-spacing: 0.455px;
-    text-transform: capitalize;
-    margin-top: 10px;
-    margin-bottom: 5px;
-`;
-
-const StickyExpandedRow = styled.div`
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    padding: 20px 0;
-    margin: auto;
-    width: calc(100% - 55px);
-    border-top: 1px solid ${(props) => props.theme.background.primary};
-`;
-
-const Cell = styled.div<{ hide?: boolean }>`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex: 1;
-    width: 100%;
-`;
-
-const StickyCell = styled(Cell)<{ first?: boolean; last?: boolean; hide?: boolean }>`
-    color: ${(props) => props.theme.button.textColor.primary};
-    width: 100%;
-    margin-right: ${(props) => (props.last ? '10px' : '0')};
-
-    border-top-left-radius: ${(props) => (props.first ? '8px' : '0')};
-    border-bottom-left-radius: ${(props) => (props.first ? '8px' : '0')};
-
-    border-top-right-radius: ${(props) => (props.last ? '8px' : '0')};
-    border-bottom-right-radius: ${(props) => (props.last ? '8px' : '0')};
-    @media screen and (max-width: ${ScreenSizeBreakpoint.SMALL}px) {
-        display: ${(props) => (props.hide ? 'none' : 'flex')};
-    }
-`;
 
 export default Leaderboard;
--- src/pages/Staking/Leaderboard/styled-components.ts
+++ src/pages/Staking/Leaderboard/styled-components.ts
@@ -1,5 +1,6 @@
+import { ScreenSizeBreakpoint } from 'enums/ui';
 import styled from 'styled-components';
-import { FlexDivColumn } from 'styles/common';
+import { FlexDivCentered, FlexDivColumn } from 'styles/common';
 
 export const Container = styled.div`
     color: ${(props) => props.theme.textColor.tertiary};
@@ -50,5 +51,90 @@ export const LeaderboardBreakdownTitle = styled(FlexDivColumn)`
     > i {
         font-size: 25px;
         margin-bottom: 5px;
+    }
+`;
+export const TableText = styled.p`
+    color: ${(props) => props.theme.textColor.primary};
+    font-size: 14px;
+    font-style: normal;
+    line-height: 110%;
+    @media screen and (max-width: ${ScreenSizeBreakpoint.SMALL}px) {
+        font-size: 13px;
+    }
+`;
+export const StickyRow = styled.div`
+    display: flex;
+    margin-bottom: 10px;
+    gap: 4px;
+    cursor: pointer;
+
+    ${TableText} {
+        color: ${(props) => props.theme.button.textColor.primary};
+    }
+
+    @media screen and (max-width: ${ScreenSizeBreakpoint.SMALL}px) {
+        height: 30px;
+    }
+`;
+export const StickyRowWrapper = styled.div`
+    background: ${(props) => props.theme.background.primary};
+    width: 100%;
+    border-radius: 8px;
+    box-shadow: 0px 10px 15px -5px rgba(0, 0, 0, 0.46);
+`;
+export const StickyRowFlex = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 40px;
+    border-radius: 8px;
+`;
+export const Icon = styled.i<{ dark?: boolean }>`
+    font-size: 32px;
+    color: ${(props) => (props.dark ? props.theme.background.primary : props.theme.textColor.primary)};
+    margin-right: 6px;
+`;
+export const FlexWrapper = styled(FlexDivCentered)`
+    flex-direction: column;
+`;
+export const Label = styled.span<{ dark?: boolean }>`
+    color: ${(props) => (props.dark ? props.theme.background.primary : props.theme.textColor.secondary)};
+    font-size: 13px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 100%;
+    letter-spacing: 0.455px;
+    text-transform: capitalize;
+    margin-top: 10px;
+    margin-bottom: 5px;
+`;
+export const StickyExpandedRow = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 20px 0;
+    margin: auto;
+    width: calc(100% - 55px);
+    border-top: 1px solid ${(props) => props.theme.background.primary};
+`;
+const Cell = styled.div<{ hide?: boolean }>`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+    width: 100%;
+`;
+export const StickyCell = styled(Cell)<{ first?: boolean; last?: boolean; hide?: boolean }>`
+    color: ${(props) => props.theme.button.textColor.primary};
+    width: 100%;
+    margin-right: ${(props) => (props.last ? '10px' : '0')};
+
+    border-top-left-radius: ${(props) => (props.first ? '8px' : '0')};
+    border-bottom-left-radius: ${(props) => (props.first ? '8px' : '0')};
+
+    border-top-right-radius: ${(props) => (props.last ? '8px' : '0')};
+    border-bottom-right-radius: ${(props) => (props.last ? '8px' : '0')};
+    @media screen and (max-width: ${ScreenSizeBreakpoint.SMALL}px) {
+        display: ${(props) => (props.hide ? 'none' : 'flex')};
     }
 `;
