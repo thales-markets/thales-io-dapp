be1c5840be25228ecee2f638a4dc8e5930a5b6fb
vojin5rovic <vojinp13@gmail.com>
2024-03-20 11:48:57 +0100

    Fix stable balance query

--- src/queries/walletBalances.ts/useStableBalanceQuery.ts
+++ src/queries/walletBalances.ts/useStableBalanceQuery.ts
@@ -1,17 +1,17 @@
-import { CRYPTO_CURRENCY_MAP } from 'constants/currency';
+import { DEFAULT_COLLATERALS } from 'constants/currency';
 import QUERY_KEYS from 'constants/queryKeys';
-import { Network } from 'enums/network';
-import { useQuery, UseQueryOptions } from 'react-query';
-import { Coins, COLLATERAL_DECIMALS } from 'thales-utils';
+import { UseQueryOptions, useQuery } from 'react-query';
+import { COLLATERAL_DECIMALS, Coins } from 'thales-utils';
+import { SupportedNetwork } from 'types/network';
 import networkConnector from 'utils/networkConnector';
 
-const useStableBalanceQuery = (walletAddress: string, networkId: Network, options?: UseQueryOptions<any>) => {
+const useStableBalanceQuery = (walletAddress: string, networkId: SupportedNetwork, options?: UseQueryOptions<any>) => {
     return useQuery<any>(
         QUERY_KEYS.WalletBalances.StableCoinBalance(walletAddress ?? '', networkId),
         async () => {
             try {
                 const collateral = networkConnector.collateral;
-                const collateralKey = CRYPTO_CURRENCY_MAP.sUSD;
+                const collateralKey = DEFAULT_COLLATERALS[networkId];
 
                 let usdBalance = await collateral?.balanceOf(walletAddress);
                 usdBalance = usdBalance
