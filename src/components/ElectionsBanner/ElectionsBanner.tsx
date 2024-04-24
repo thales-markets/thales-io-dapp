f0a95fd7d3e5f04a636939e03673191a5b46e7aa
makiBaraba <markovic.ofsec@gmail.com>
2024-04-24 10:44:50 +0200

    Padding change and text change

--- src/components/ElectionsBanner/ElectionsBanner.tsx
+++ src/components/ElectionsBanner/ElectionsBanner.tsx
@@ -1,18 +1,19 @@
 import ROUTES from 'constants/routes';
 import { ScreenSizeBreakpoint } from 'enums/ui';
 import React from 'react';
-import { Trans } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 import { FlexDiv } from 'styles/common';
 import SPAAnchor from '../SPAAnchor';
 
 const ElectionsBanner: React.FC = () => {
+    const { t } = useTranslation();
     return (
-        <SPAAnchor href={ROUTES.DAO.Home}>
+        <SPAAnchor
+            href={`${ROUTES.DAO.Home}/thalescouncil.eth/0x6c34802e090ccf84e06e9c6616402b9add95aa4475e1fa0dd90a831bcf77bc47`}
+        >
             <Container>
-                <Label>
-                    <Trans i18nKey={'common.banner.elections-banner-message'} components={{ bold: <Bold /> }} />
-                </Label>
+                <Label>{t('common.banner.elections-banner-message')}</Label>
             </Container>
         </SPAAnchor>
     );
@@ -42,11 +43,6 @@ const Label = styled.span`
     font-style: normal;
     font-weight: 800;
     text-transform: uppercase;
-`;
-
-const Bold = styled.span`
-    font-weight: bold;
-    text-decoration: underline;
 `;
 
 export default ElectionsBanner;
